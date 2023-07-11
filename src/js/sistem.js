c32fdfa21cc3e8e31be88d43fe8f57e5ef336702
LUIS DAS ARTIMANHAS <luisaugustodesouza785@gmail.com>
2023-07-11 15:06:13 -0300

    updated

--- src/js/sistem.js
+++ src/js/sistem.js
@@ -1,17 +1,25 @@
 
 // array system
-function save(array) {
-
+function save(dados) {
+    // obtem o banco de dados
+    data = readData(array);
+    // salve os dados novamente
+    data.push(dados)
+    // retorne o banco de dados
+    return readData(data)
 }
-function readData(nome){
+function readData(nome) {
     var data = localStorage.getItem(nome);
-    if(!data){
+    // Verifica se o banco de dados não exitse
+    if (!data) {
+        // define o banco de dados como o padrão se não encntrar
         data = []
     }
+    // calculos para medição de uso de dados
     const dataString = JSON.stringify(data)
     const bytes = dataString.length
-    const kBytes = (bytes/1000).toFixed(2)
-    const mBytes = (kBytes/1000).toFixed(2)
+    const kBytes = (bytes / 1000).toFixed(2)
+    const mBytes = (kBytes / 1000).toFixed(2)
 
     // apenas para saber o que esta acontecendo
     console.log("-------DATA-SYSTEM------");
@@ -21,4 +29,6 @@ function readData(nome){
     console.log("TAMANHO(Bytes): " + bytes);
     console.log("TAMANHO(KB): " + kBytes);
     console.log("TAMANHO(MB): " + mBytes);
-  }
+    // retorna os dados lidos
+    return data
+}
